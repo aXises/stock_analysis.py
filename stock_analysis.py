18a184b4e1738a0804c5a56be1c079c0b9043709
aXises <leletonyli.13944@yahoo.com.au>
2017-04-19 21:13:47 +1000

    Added GapUp Analyser class and exception handling for loaders

--- stock_analysis.py
+++ stock_analysis.py
@@ -12,16 +12,19 @@ class LoadCSV(Loader):
     def _process(self, file):
         for data in file:
             datalist = data.split(",")
+            if len(datalist) == 7 and len(datalist[0]) >= 3:
+                stock = all_stocks.get_stock(datalist[0])
+                day_data = TradingData(str(datalist[1]),
+                                       float(datalist[2]),
+                                       float(datalist[3]),
+                                       float(datalist[4]),
+                                       float(datalist[5]),
+                                       int(datalist[6]))
 
-            stock = all_stocks.get_stock(datalist[0])
-            day_data = TradingData(str(datalist[1]),
-                                   float(datalist[2]),
-                                   float(datalist[3]),
-                                   float(datalist[4]),
-                                   float(datalist[5]),
-                                   int(datalist[6]))
+                stock.add_day_data(day_data)
+            else:
+                raise RuntimeError
 
-            stock.add_day_data(day_data)
 
 class LoadTriplet(Loader):
 
@@ -36,35 +39,43 @@ class LoadTriplet(Loader):
         for data in file:
             datalist.append(data)
 
-        for x in range(0, len(datalist), 6):
-            datalist_split.append(datalist[x:x+6])
+        if len(datalist) % 6 == 0:
 
-        for data in datalist_split:
-            for string in data:
-                x = string.split(":")
-                valuelist.append(x[2])
-                codelist.append(x[0])
+            for x in range(0, len(datalist), 6):
+                datalist_split.append(datalist[x:x+6])
 
-        for x in range(0, len(valuelist), 6):
-            stocklist.append(valuelist[x:x+6])
+            for data in datalist_split:
+                for string in data:
+                    x = string.split(":")
+                    valuelist.append(x[2])
+                    codelist.append(x[0])
 
-        for x in range(0, len(codelist), 6):
-            codelist_split.append(codelist[x:x+6])
+            for code in codelist:
+                if len(code) < 3:
+                    raise RuntimeError
 
-        c = 0
-        for x in codelist_split:
-            stocklist[c].append(x[0])
-            c += 1
+            for x in range(0, len(valuelist), 6):
+                stocklist.append(valuelist[x:x+6])
 
-        for data in stocklist:
-            stock = all_stocks.get_stock(data[6])
-            day_data = TradingData(str(data[0]),
-                                   float(data[1]),
-                                   float(data[2]),
-                                   float(data[3]),
-                                   float(data[4]),
-                                   int(data[5]))
-            stock.add_day_data(day_data)
+            for x in range(0, len(codelist), 6):
+                codelist_split.append(codelist[x:x+6])
+
+            c = 0
+            for x in codelist_split:
+                stocklist[c].append(x[0])
+                c += 1
+
+            for data in stocklist:
+                stock = all_stocks.get_stock(data[6])
+                day_data = TradingData(str(data[0]),
+                                       float(data[1]),
+                                       float(data[2]),
+                                       float(data[3]),
+                                       float(data[4]),
+                                       int(data[5]))
+                stock.add_day_data(day_data)
+        else:
+            raise RuntimeError
 
 
 class HighLow(Analyser):
@@ -84,20 +95,17 @@ class HighLow(Analyser):
     def result(self):
         self._total_high.sort()
         self._total_low.sort()
-        return (self._total_high[-1], self._total_low[0])
+        return self._total_high[-1], self._total_low[0]
 
 
 class MovingAverage(Analyser):
 
     def __init__(self, num_days):
         self._num_days = num_days
-        self._closing = 0
         self._closing_total = 0
         self._data = []
 
     def process(self, day):
-        self._closing = day.get_high()
-        self._date = day.get_date()
         self._data.append(day.get_high())
 
         if len(self._data) >= self._num_days:
@@ -106,16 +114,39 @@ class MovingAverage(Analyser):
                 self._closing_total = sum(self._data)
 
     def reset(self):
-        self._closing = 0
         self._closing_total = 0
         self._data = []
 
     def result(self):
         return self._closing_total / self._num_days
 
+
 class GapUp(Analyser):
-    def __init__(self):
-        pass
+
+    def __init__(self, delta):
+        self._delta = delta
+        self._opening = []
+        self._closing = []
+        self._date = 0
+
+    def process(self, day):
+        self._opening.append(day.get_open())
+        self._closing.append(day.get_close())
+
+        if len(self._closing) > 1:
+            if self._opening[-1] - self._closing[-2] > self._delta:
+                self._date = day
+        else:
+            self._date = None
+
+    def reset(self):
+        self._opening = []
+        self._closing = []
+        self._date = 0
+
+    def result(self):
+        return self._date
+
 
 LoadCSV("data_files/march1.csv", all_stocks)
 LoadCSV("data_files/march2.csv", all_stocks)
@@ -128,49 +159,17 @@ LoadTriplet("data_files/feb3.trp", all_stocks)
 LoadTriplet("data_files/feb4.trp", all_stocks)
 
 volume = stocks.AverageVolume()
-stock = all_stocks.get_stock("ADV")
-
+code = "1AD"
+stock = all_stocks.get_stock(code)
 stock.analyse(volume)
-print("Average Volume of ADV is", volume.result())
+print("Average Volume of", code ,"is", volume.result())
 high_low = HighLow()
 stock.analyse(high_low)
-print("Highest & Lowest trading price of ADV is", high_low.result())
-moving_average = MovingAverage(10)
+print("Highest & Lowest trading price of", code ,"is", high_low.result())
+moving_average = MovingAverage(4)
 stock.analyse(moving_average)
-print("Moving average of ADV over last 10 days is {0:.2f}"
-      .format(moving_average.result()))
-
-"""
-stocks_loaded = all_stocks._all_stocks.keys()
-print(stocks_loaded)
-print(len(stocks_loaded))
-print(all_stocks._all_stocks.get('ADV'))
-"""
-
-"""
-#debug
-print(code, 'date is:', stock.get_day_data(date).get_date(), 'type is', type(stock.get_day_data(date).get_date()))
-print(code, 'open is:',stock.get_day_data(date).get_open(), 'type is', type(stock.get_day_data(date).get_open()))
-print(code, 'high is:',stock.get_day_data(date).get_high(), 'type is', type(stock.get_day_data(date).get_high()))
-print(code, 'low is:',stock.get_day_data(date).get_low(), 'type is', type(stock.get_day_data(date).get_low()))
-print(code, 'close is:',stock.get_day_data(date).get_close(), 'type is', type(stock.get_day_data(date).get_close()))
-print(code, 'volume is:',stock.get_day_data(date).get_volume(),'type is', type(stock.get_day_data(date).get_volume()))
-
-
-highlow = HighLow()
-stock.analyse(highlow)
-print("HighLow of", code, "is", highlow.result())
-
-date = '20170227'
-code = "ADV"
-
-stock = all_stocks.get_stock(code)
-
-movingaverage = MovingAverage(2)
-stock.analyse(movingaverage)
-print("Moving Average of", code, "is", movingaverage.result())
-
-#stock.analyse(volume)
-#print("Average Volume of", code, "is", volume.result())
-"""
-
+print("Moving average of ", code ,"over last n days is {0:.2f}"
+      .format(moving_average.result()), moving_average.result())
+gap_up = GapUp(0.0009)
+stock.analyse(gap_up)
+print("Last gap up date of", code,"is", gap_up.result.get_date())
